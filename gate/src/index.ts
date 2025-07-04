e59a82f148ba1fb5272ce07d9dea368098f6b44d
Josh Nussbaum <joshnuss@gmail.com>
2025-07-03 20:01:07 -0400

    Annotate code

--- gate/src/index.ts
+++ gate/src/index.ts
@@ -2,8 +2,10 @@ import Stripe from 'stripe'
 import { SerialPort } from 'serialport'
 import { ReadlineParser } from '@serialport/parser-readline'
 
+// create the Stripe API client
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
+// utility function to find the NFC reader
 async function find_nfc_reader(): Promise<string | undefined> {
   const ports = await SerialPort.list()
 
@@ -13,36 +15,48 @@ async function find_nfc_reader(): Promise<string | undefined> {
   return port?.path
 }
 
-// flash LEDs green, and play a beep
+// function to flash LEDs green, and play a beep
 function success(port: SerialPort) {
   port.write('?LEDR 00FF00,100,100,4\n')
   port.write('?BEEPR 100,100,2\n')
 }
 
-// flash LEDs red, and play a beep
+// function to flash LEDs red, and play a beep
 function error(port: SerialPort) {
   port.write('?LEDR FF0000,1000,100,1\n')
   port.write('?BEEPR 600,100,1,300\n')
 }
 
+// figure out the default path, on Linux it's usually /dev/ttyACM0
 const path = process.argv.length > 2 ? process.argv[2] : await find_nfc_reader()
 
+// raise error when no NFC reader is found
 if (!path) throw new Error('No NFC reader found! Please make sure NFC reader is connected.')
 
+// create serial port client
 const port = new SerialPort({ path, baudRate: 9600 })
+// use Readline so that we get full lines
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
+// handle new data from serial port
 parser.on('data', async (subscription_id) => {
+  // ignore useless messages
   if (subscription_id == 'OK') return
+
+  // otherwise, the data is the Stripe subscription id
   console.log(subscription_id)
 
+  // get the subscription and customer records
   const subscription = await stripe.subscriptions.retrieve(subscription_id)
   const customer = await stripe.customers.retrieve(subscription.customer as string)
 
+  // check if subscription is active
   if (subscription.status === 'active' && !customer.deleted) {
+    // flash LEDs green and play sound
     success(port)
     console.log(`Welcome ${ customer.name }!`)
   } else {
+    // flash LEDs red and play sound
     error(port)
     console.log(`Access denied. status=${subscription?.status}`)
   }
--- website/src/index.ts
+++ website/src/index.ts
@@ -6,6 +6,7 @@ import path from 'node:path'
 import Stripe from 'stripe'
 import { PassNinjaClient } from '@passninja/passninja-js'
 
+// extract env vars
 const {
   STRIPE_SECRET_KEY,
   STRIPE_PRICE_ID,
@@ -14,13 +15,23 @@ const {
   PASSNINJA_API_KEY,
   PASSNINJA_PASS_TYPE,
 } = process.env
+
+// create Stripe API client
 const stripe = new Stripe(STRIPE_SECRET_KEY as string)
+
+// prepare the success_url
 const success_url = new URL('/success?session_id={CHECKOUT_SESSION_ID}', SITE_URL).toString()
+
+// create the Pass Ninja API client
 const passNinja = new PassNinjaClient(PASSNINJA_ACCOUNT_ID as string, PASSNINJA_API_KEY as string)
 
+// use Hono to handle web requests
 const app = new Hono()
 
+// when user visits /subscribe, a Stripe checkout session is created and the user is redirected
 app.post('/subscribe', async (c) => {
+
+  // create checkout session
   const session = await stripe.checkout.sessions.create({
     mode: 'subscription',
     success_url,
@@ -31,33 +42,44 @@ app.post('/subscribe', async (c) => {
 
   if (!session?.url) throw new Error('Session not found')
 
+  // redirect user to checkout
   return c.redirect(session.url)
 })
 
+// when user visits /success (after checkout completes), provision the pass
 app.get('/success', async (c) => {
   const session_id = c.req.query('session_id')
   
   if (!session_id) throw new Error('Missing session_id param')
 
+  // get the Stripe checkout session
   const session = await stripe.checkout.sessions.retrieve(session_id)
 
+  // get the Stripe subscription
   const subscription = await stripe.subscriptions.retrieve(session.subscription as string)
 
+  // verify the subscription is active
   if (subscription?.status !== 'active') throw new Error('Subscription was not successful')
 
+  // issue a new pass
   const pass = await passNinja.pass.create(
     PASSNINJA_PASS_TYPE,
     {
       name: session.customer_details!.name,
       email: session.customer_details!.email,
+
+      // save the subscription id in the pass
       "nfc-message": session.subscription,
+
       since: "January 2025"
     }
   )
 
+  // redirect the user to the pass
   return c.redirect(pass.url)
 })
 
+// handle static files like /index.html
 app.use('/*', serveStatic({
   root: './static',
   getContent: async path => {
@@ -66,9 +88,7 @@ app.use('/*', serveStatic({
   }
 }))
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
+// run the server on port 3000
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
